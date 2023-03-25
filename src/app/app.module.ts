087b4c03c26ccc8da604038e121ad3688f79ddd1
meriemdhaher <128425485+meriemdhaher@users.noreply.github.com>
2023-03-25 04:38:32 +0100

    integration template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { HeroComponent } from './page/hero/hero.component';
+import { AboutComponent } from './page/about/about.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { SpecialsComponent } from './page/specials/specials.component';
+import { EventsComponent } from './page/events/events.component';
+import { BookTableComponent } from './page/book-table/book-table.component';
+import { TestimonialsComponent } from './page/testimonials/testimonials.component';
+import { GalleryComponent } from './page/gallery/gallery.component';
+import { ChefsComponent } from './page/chefs/chefs.component';
+import { ContactComponent } from './page/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    TopbarComponent,
+    HeroComponent,
+    AboutComponent,
+    MenuComponent,
+    SpecialsComponent,
+    EventsComponent,
+    BookTableComponent,
+    TestimonialsComponent,
+    GalleryComponent,
+    ChefsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
